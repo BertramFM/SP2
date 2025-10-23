94262ae25b53e9088b117946693b98fc58a5ea13
BertramFM <160260021+BertramFM@users.noreply.github.com>
2025-10-23 14:58:03 +0200

    add

---
+++ Bibliotekspenge/src/model/Title.java
@@ -0,0 +1,35 @@
+package model;
+
+public abstract class Title {
+    private String title;
+    private String literatureType;
+    private static final double RATE = 0.067574;
+
+    public Title(String title, String literatureType) {
+        this.title = title;
+        this.literatureType = literatureType;
+    }
+
+    public double calculateRoyalty(){
+        return calculatePoints() * RATE;
+    }
+
+    protected abstract double calculatePoints();
+
+    protected double convertLiteratureType(){
+        switch (literatureType.toUpperCase()){
+            case "BI":
+            case "TE":
+                return 3;
+            case "LYRIK":
+                return 6;
+            case "SKØN":
+                return 1.7;
+            case "FAG":
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+}
---
+++ Bibliotekspenge/src/model/physical/AudioBook.java
@@ -0,0 +1,19 @@
+package model.physical;
+
+import model.Title;
+
+public class AudioBook extends Title {
+
+    private int durationInMinutes;
+    private int copies;
+
+    public AudioBook(String title, String literatureType, int durationInMinutes, int copies) {
+        super(title, literatureType);
+        this.durationInMinutes = durationInMinutes;
+        this.copies = copies;
+    }
+
+    public double calculatePoints(){
+        return (durationInMinutes * 0.5) * convertLiteratureType() * copies;
+    }
+}
